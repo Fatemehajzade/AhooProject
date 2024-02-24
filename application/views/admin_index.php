121fac29dcc8956f2652033379d5cdcf3f37964f
MohadeseNikoogoftar <nikomohdse@gmail.com>
2024-02-24 19:28:26 +0330

    edit_admiin_index

--- application/views/admin_index.php
+++ application/views/admin_index.php
@@ -371,7 +371,7 @@ foreach ($info as $key => $row) {
 	
 	
 	
-	
+//mohi2
 <script src="<?php echo base_url() ?>js/all.min.js"></script>
 <script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
 <script src="<?php echo base_url() ?>js/jquery-3.3.1.min.js"></script>
